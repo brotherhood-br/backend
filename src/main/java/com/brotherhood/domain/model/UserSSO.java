d62ad3e10d5528e65abb3a50dd8b446710e4cc00
Samuel Guedes <sguedes46@gmail.com>
2023-06-08 19:31:25 -0300

    Fix - userType on create

--- src/main/java/com/brotherhood/domain/model/UserSSO.java
+++ src/main/java/com/brotherhood/domain/model/UserSSO.java
@@ -1,114 +1,16 @@
 package com.brotherhood.domain.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class UserSSO {
     private String userId;
     private String email;
     private String pictureUrl;
-
-    public UserSSO(String userId, String email, String pictureUrl) {
-        this.userId = userId;
-        this.email = email;
-        this.pictureUrl = pictureUrl;
-    }
-
-    public UserSSO() {
-    }
-
-    public static UserSSOBuilder builder() {
-        return new UserSSOBuilder();
-    }
-
-    public String getUserId() {
-        return this.userId;
-    }
-
-    public String getEmail() {
-        return this.email;
-    }
-
-    public String getPictureUrl() {
-        return this.pictureUrl;
-    }
-
-    public void setUserId(String userId) {
-        this.userId = userId;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public void setPictureUrl(String pictureUrl) {
-        this.pictureUrl = pictureUrl;
-    }
-
-    public boolean equals(final Object o) {
-        if (o == this) return true;
-        if (!(o instanceof UserSSO)) return false;
-        final UserSSO other = (UserSSO) o;
-        if (!other.canEqual((Object) this)) return false;
-        final Object this$userId = this.getUserId();
-        final Object other$userId = other.getUserId();
-        if (this$userId == null ? other$userId != null : !this$userId.equals(other$userId)) return false;
-        final Object this$email = this.getEmail();
-        final Object other$email = other.getEmail();
-        if (this$email == null ? other$email != null : !this$email.equals(other$email)) return false;
-        final Object this$pictureUrl = this.getPictureUrl();
-        final Object other$pictureUrl = other.getPictureUrl();
-        if (this$pictureUrl == null ? other$pictureUrl != null : !this$pictureUrl.equals(other$pictureUrl))
-            return false;
-        return true;
-    }
-
-    protected boolean canEqual(final Object other) {
-        return other instanceof UserSSO;
-    }
-
-    public int hashCode() {
-        final int PRIME = 59;
-        int result = 1;
-        final Object $userId = this.getUserId();
-        result = result * PRIME + ($userId == null ? 43 : $userId.hashCode());
-        final Object $email = this.getEmail();
-        result = result * PRIME + ($email == null ? 43 : $email.hashCode());
-        final Object $pictureUrl = this.getPictureUrl();
-        result = result * PRIME + ($pictureUrl == null ? 43 : $pictureUrl.hashCode());
-        return result;
-    }
-
-    public String toString() {
-        return "UserSSO(userId=" + this.getUserId() + ", email=" + this.getEmail() + ", pictureUrl=" + this.getPictureUrl() + ")";
-    }
-
-    public static class UserSSOBuilder {
-        private String userId;
-        private String email;
-        private String pictureUrl;
-
-        UserSSOBuilder() {
-        }
-
-        public UserSSOBuilder userId(String userId) {
-            this.userId = userId;
-            return this;
-        }
-
-        public UserSSOBuilder email(String email) {
-            this.email = email;
-            return this;
-        }
-
-        public UserSSOBuilder pictureUrl(String pictureUrl) {
-            this.pictureUrl = pictureUrl;
-            return this;
-        }
-
-        public UserSSO build() {
-            return new UserSSO(userId, email, pictureUrl);
-        }
-
-        public String toString() {
-            return "UserSSO.UserSSOBuilder(userId=" + this.userId + ", email=" + this.email + ", pictureUrl=" + this.pictureUrl + ")";
-        }
-    }
 }
--- src/main/java/com/brotherhood/domain/service/CreateUserService.java
+++ src/main/java/com/brotherhood/domain/service/CreateUserService.java
@@ -5,6 +5,7 @@ import com.brotherhood.domain.dataprovider.GetBrotherhoodByInviteTokenDataProvid
 import com.brotherhood.domain.dataprovider.GetUserInfoFromGoogleDataProvider;
 import com.brotherhood.domain.entity.BrotherhoodEntity;
 import com.brotherhood.domain.entity.UserEntity;
+import com.brotherhood.domain.entity.UserTypeEntity;
 import com.brotherhood.domain.model.UserSSO;
 import com.brotherhood.model.CreateUser;
 import lombok.RequiredArgsConstructor;
@@ -23,12 +24,14 @@ public class CreateUserService {
         UserEntity user = getUserEntity(ssoToken, createUser);
         BrotherhoodEntity brotherhood = getBrotherhoodByInviteTokenDataProvider.find(createUser.brotherhoodToken());
         user.setBrotherhood(brotherhood);
+        user.setType(UserTypeEntity.RESIDENT);
         createUserDataProvider.save(user);
     }
 
     public void createAdminUser(String ssoToken, CreateUser createUser, BrotherhoodEntity brotherhood) {
         UserEntity user = getUserEntity(ssoToken, createUser);
         user.setBrotherhood(brotherhood);
+        user.setType(UserTypeEntity.ADMIN);
         createUserDataProvider.save(user);
     }
 
@@ -39,7 +42,8 @@ public class CreateUserService {
                 .picture(userSSO.getPictureUrl())
                 .id(UUID.randomUUID())
                 .email(userSSO.getEmail())
-                .googleId(userSSO.getUserId()).phone(createUser.getPhone())
+                .googleId(userSSO.getUserId())
+                .phone(createUser.getPhone())
                 .name(createUser.getName())
                 .birthdate(createUser.getBirthDate())
                 .build();

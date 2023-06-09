a3ebd3960d3f7c32d3e966a101f542834ccd0f50
Rafael Marotta <rafaelmarottag@gmail.com>
2023-06-09 01:46:02 -0300

    Finishing swagger.yaml

--- src/main/java/com/brotherhood/controller/BrotherhoodController.java
+++ src/main/java/com/brotherhood/controller/BrotherhoodController.java
@@ -3,6 +3,7 @@ package com.brotherhood.controller;
 import com.brotherhood.api.BrotherhoodApi;
 import com.brotherhood.domain.service.CreateBrotherhoodService;
 import com.brotherhood.model.Brotherhood;
+import com.brotherhood.model.BrotherhoodAdminPage;
 import com.brotherhood.model.CreateBrotherhood;
 import io.micronaut.http.HttpResponse;
 import io.micronaut.http.annotation.Controller;
@@ -14,16 +15,24 @@ import java.util.UUID;
 @RequiredArgsConstructor
 public class BrotherhoodController implements BrotherhoodApi {
     private final CreateBrotherhoodService createBrotherhoodService;
-
-    @Override
-    public HttpResponse<Brotherhood> brotherhoodsIdGet(UUID id) {
-        return HttpResponse.ok(new Brotherhood().banner("Hello World!"));
-    }
-
     @Override
     public HttpResponse<Object> createBrotherHood(String ssoToken, CreateBrotherhood createBrotherhood) {
         createBrotherhoodService.create(ssoToken, createBrotherhood);
         return HttpResponse.noContent();
     }
 
+    @Override
+    public HttpResponse<BrotherhoodAdminPage> getBrotherhoodAdminPage(String ssoToken) {
+        return null;
+    }
+
+    @Override
+    public HttpResponse<Brotherhood> getBrotherhoodById(UUID id) {
+        return HttpResponse.ok(new Brotherhood().banner("Hello World!"));
+    }
+
+    @Override
+    public HttpResponse<Object> updateBrotherHood(String ssoToken, UUID id, CreateBrotherhood createBrotherhood) {
+        return null;
+    }
 }
--- src/main/java/com/brotherhood/controller/UsersController.java
+++ src/main/java/com/brotherhood/controller/UsersController.java
@@ -3,9 +3,13 @@ package com.brotherhood.controller;
 import com.brotherhood.api.UsersApi;
 import com.brotherhood.domain.service.CreateUserService;
 import com.brotherhood.model.CreateUser;
+import com.brotherhood.model.UpdateUser;
+import com.brotherhood.model.UserCard;
 import io.micronaut.http.HttpResponse;
 import io.micronaut.http.annotation.Controller;
 import lombok.RequiredArgsConstructor;
+
+import java.util.UUID;
 
 @Controller
 @RequiredArgsConstructor
@@ -16,5 +20,20 @@ public class UsersController implements UsersApi {
     public HttpResponse<Object> createUser(String ssoToken, CreateUser createUser) {
         createUserService.createInvitedUser(ssoToken, createUser);
         return HttpResponse.noContent();
+    }
+
+    @Override
+    public HttpResponse<Object> deleteUser(String ssoToken, UUID id) {
+        return null;
+    }
+
+    @Override
+    public HttpResponse<UserCard> getUsers(String ssoToken) {
+        return null;
+    }
+
+    @Override
+    public HttpResponse<Object> updateUser(String ssoToken, UUID id, UpdateUser updateUser) {
+        return null;
     }
 }
